d1887b6224481150ef419436adc5ffb923c191da
Joseph <Joseph.DeGois17@gmail.com>
2022-10-18 17:01:20 -0400

    Added constructor, getter, setter, to string to user

--- src/main/java/com/example/recipeproject/users/User.java
+++ src/main/java/com/example/recipeproject/users/User.java
@@ -1,4 +1,99 @@
 package com.example.recipeproject.users;
 
 public class User {
+    private Long id;
+    private String user_name;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String country;
+    private String province;
+
+    public User(Long id, String user_name, String first_name, String last_name, String email, String country, String province) {
+        this.id = id;
+        this.user_name = user_name;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.country = country;
+        this.province = province;
+    }
+
+    public User(String user_name, String first_name, String last_name, String email, String country, String province) {
+        this.user_name = user_name;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.country = country;
+        this.province = province;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", user_name='" + user_name + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", province='" + province + '\'' +
+                '}';
+    }
 }
